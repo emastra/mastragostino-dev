7d26a33f2c9f8bcf2d937056863db439ba0e8bb3
emastra <emiliano.mastra@gmail.com>
2025-12-17 12:45:33 +0100

    Adapted form for new approach (removed elements)

--- pages/miniaudit.tsx
+++ pages/miniaudit.tsx
@@ -130,11 +130,11 @@ function FormSection() {
   const [formData, setFormData] = useState({
     name: '',
     agency: '',
-    contactType: '',
+    // contactType: '',
     // whatsapp: '',
     phone: '',
     email: '',
-    task: '',
+    // task: '',
   });
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -149,7 +149,7 @@ function FormSection() {
         agency: (fd.get('agency') as string) || '',
         phone: (fd.get('phone') as string) || '',
         email: (fd.get('email') as string) || '',
-        task: (fd.get('task') as string) || '',
+        // task: (fd.get('task') as string) || '',
       };
 
       const res = await fetch('/api/miniaudit-form-submit', {
@@ -189,17 +189,7 @@ function FormSection() {
   };
 
   // Get the appropriate input field based on contactType
-  const getContactInputField = () => {
-    const commonProps = {
-      required: true,
-      value: formData[formData.contactType as keyof typeof formData] || '',
-      onChange: handleChange,
-      className:
-        'w-full rounded-lg border border-neutral-300 bg-white py-3 pl-10 pr-4 text-neutral-900 placeholder-neutral-400 transition-all focus:border-primary-500 focus:outline-none focus:ring-2 focus:ring-primary-500/20 dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100 dark:placeholder-neutral-500',
-    };
-
-    switch (formData.contactType) {
-      /* case 'whatsapp':
+  /* case 'whatsapp':
         return (
           <input
             {...commonProps}
@@ -209,6 +199,17 @@ function FormSection() {
             placeholder="3381234567"
           />
         ); */
+  /* const getContactInputField = () => {
+    const commonProps = {
+      required: true,
+      value: formData[formData.contactType as keyof typeof formData] || '',
+      onChange: handleChange,
+      className:
+        'w-full rounded-lg border border-neutral-300 bg-white py-3 pl-10 pr-4 text-neutral-900 placeholder-neutral-400 transition-all focus:border-primary-500 focus:outline-none focus:ring-2 focus:ring-primary-500/20 dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100 dark:placeholder-neutral-500',
+    };
+
+    switch (formData.contactType) {
+      // whatsapp here
       case 'phone':
         return (
           <input
@@ -232,11 +233,9 @@ function FormSection() {
       default:
         return null;
     }
-  };
+  }; */
 
-  const getContactIcon = () => {
-    switch (formData.contactType) {
-      /* case 'whatsapp':
+  /* case 'whatsapp':
         return (
           <svg
             className="h-5 w-5 text-neutral-400"
@@ -247,6 +246,9 @@ function FormSection() {
             <path d="M26.576 5.363c-2.69-2.69-6.406-4.354-10.511-4.354-8.209 0-14.865 6.655-14.865 14.865 0 2.732 0.737 5.291 2.022 7.491l-0.038-0.070-2.109 7.702 7.879-2.067c2.051 1.139 4.498 1.809 7.102 1.809h0.006c8.209-0.003 14.862-6.659 14.862-14.868 0-4.103-1.662-7.817-4.349-10.507l0 0zM16.062 28.228h-0.005c-0 0-0.001 0-0.001 0-2.319 0-4.489-0.64-6.342-1.753l0.056 0.031-0.451-0.267-4.675 1.227 1.247-4.559-0.294-0.467c-1.185-1.862-1.889-4.131-1.889-6.565 0-6.822 5.531-12.353 12.353-12.353s12.353 5.531 12.353 12.353c0 6.822-5.53 12.353-12.353 12.353h-0zM22.838 18.977c-0.371-0.186-2.197-1.083-2.537-1.208-0.341-0.124-0.589-0.185-0.837 0.187-0.246 0.371-0.958 1.207-1.175 1.455-0.216 0.249-0.434 0.279-0.805 0.094-1.15-0.466-2.138-1.087-2.997-1.852l0.010 0.009c-0.799-0.74-1.484-1.587-2.037-2.521l-0.028-0.052c-0.216-0.371-0.023-0.572 0.162-0.757 0.167-0.166 0.372-0.434 0.557-0.65 0.146-0.179 0.271-0.384 0.366-0.604l0.006-0.017c0.043-0.087 0.068-0.188 0.068-0.296 0-0.131-0.037-0.253-0.101-0.357l0.002 0.003c-0.094-0.186-0.836-2.014-1.145-2.758-0.302-0.724-0.609-0.625-0.836-0.637-0.216-0.010-0.464-0.012-0.712-0.012-0.395 0.010-0.746 0.188-0.988 0.463l-0.001 0.002c-0.802 0.761-1.3 1.834-1.3 3.023 0 0.026 0 0.053 0.001 0.079l-0-0.004c0.131 1.467 0.681 2.784 1.527 3.857l-0.012-0.015c1.604 2.379 3.742 4.282 6.251 5.564l0.094 0.043c0.548 0.248 1.25 0.513 1.968 0.74l0.149 0.041c0.442 0.14 0.951 0.221 1.479 0.221 0.303 0 0.601-0.027 0.889-0.078l-0.031 0.004c1.069-0.223 1.956-0.868 2.497-1.749l0.009-0.017c0.165-0.366 0.261-0.793 0.261-1.242 0-0.185-0.016-0.366-0.047-0.542l0.003 0.019c-0.092-0.155-0.34-0.247-0.712-0.434z"></path>
           </svg>
         ); */
+  /* const getContactIcon = () => {
+    switch (formData.contactType) {
+      // whatsapp here
       case 'phone':
         return <Phone className="h-5 w-5 text-neutral-400" />;
       case 'email':
@@ -254,7 +256,7 @@ function FormSection() {
       default:
         return <MessageCircleMore className="h-5 w-5 text-neutral-400" />;
     }
-  };
+  }; */
 
   if (formState === 'success') {
     return <SuccessMessage />;
@@ -325,7 +327,6 @@ function FormSection() {
                       type="text"
                       id="agency"
                       name="agency"
-                      required
                       value={formData.agency}
                       onChange={handleChange}
                       placeholder="Tecnocasa Ostia"
@@ -334,8 +335,62 @@ function FormSection() {
                   </div>
                 </div>
 
-                {/* Contact Type Select */}
+                {/* Email Field (SINCE NO PREFERENCE SELECT) */}
                 <div>
+                  <label
+                    htmlFor="name"
+                    className="mb-2 ml-1 block text-sm font-semibold text-neutral-700 dark:text-neutral-300"
+                  >
+                    Email *
+                  </label>
+                  <div className="relative">
+                    <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                      <Mail className="h-5 w-5 text-neutral-400" />
+                    </div>
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      placeholder="mario@esempio.com"
+                      required
+                      value={formData.name}
+                      onChange={handleChange}
+                      className="w-full rounded-lg border border-neutral-300 bg-white py-3 pl-10 pr-4 text-neutral-900 placeholder-neutral-400 transition-all focus:border-primary-500 focus:outline-none focus:ring-2 focus:ring-primary-500/20 dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100 dark:placeholder-neutral-500"
+                    />
+                  </div>
+                </div>
+
+                {/* Phone Field (SINCE NO PREFERENCE SELECT) */}
+                <div>
+                  <label
+                    htmlFor="name"
+                    className="mb-2 ml-1 block text-sm font-semibold text-neutral-700 dark:text-neutral-300"
+                  >
+                    Telefono{' '}
+                    <span className="font-normal text-neutral-500">
+                      (Opzionale)
+                    </span>
+                  </label>
+                  <div className="relative">
+                    <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                      <Phone className="h-5 w-5 text-neutral-400" />
+                    </div>
+                    <input
+                      type="tel"
+                      id="phone"
+                      name="phone"
+                      placeholder="3381234567"
+                      value={formData.agency}
+                      onChange={handleChange}
+                      className="w-full rounded-lg border border-neutral-300 bg-white py-3 pl-10 pr-4 text-neutral-900 placeholder-neutral-400 transition-all focus:border-primary-500 focus:outline-none focus:ring-2 focus:ring-primary-500/20 dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100 dark:placeholder-neutral-500"
+                    />
+                  </div>
+                </div>
+
+                {/* Contact Type Select */}
+                {/* <option value="whatsapp">WhatsApp</option> */}
+                {/* chevron indicator */}
+                {/* <div>
                   <label
                     htmlFor="contactType"
                     className="mb-2 ml-1 block text-sm font-semibold text-neutral-700 dark:text-neutral-300"
@@ -359,12 +414,12 @@ function FormSection() {
                       className="w-full appearance-none rounded-lg border border-neutral-300 bg-white py-3 pl-10 pr-4 text-neutral-900 placeholder-neutral-400 transition-all focus:border-primary-500 focus:outline-none focus:ring-2 focus:ring-primary-500/20 dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100 dark:placeholder-neutral-500"
                     >
                       <option value="">Seleziona...</option>
-                      {/* <option value="whatsapp">WhatsApp</option> */}
+                      
                       <option value="phone">Telefono</option>
                       <option value="email">Email</option>
                     </select>
 
-                    {/* chevron indicator */}
+                    
                     <div className="pointer-events-none absolute inset-y-0 right-3 flex items-center">
                       <svg
                         className="h-4 w-4 text-neutral-400"
@@ -382,10 +437,11 @@ function FormSection() {
                       </svg>
                     </div>
                   </div>
-                </div>
+                </div> */}
 
                 {/* Dynamic Contact Field - Appears after contactType selection */}
-                {formData.contactType && (
+                {/* {formData.contactType === 'whatsapp' && 'WhatsApp *'} */}
+                {/* {formData.contactType && (
                   <div
                     className="duration-250 animate-in fade-in slide-in-from-top-2"
                     style={{
@@ -396,7 +452,7 @@ function FormSection() {
                       htmlFor={formData.contactType}
                       className="mb-2 ml-1 block text-sm font-semibold text-neutral-700 dark:text-neutral-300"
                     >
-                      {/* {formData.contactType === 'whatsapp' && 'WhatsApp *'} */}
+                      
                       {formData.contactType === 'phone' && 'Telefono *'}
                       {formData.contactType === 'email' && 'Email *'}
                     </label>
@@ -407,11 +463,11 @@ function FormSection() {
                       {getContactInputField()}
                     </div>
                   </div>
-                )}
+                )} */}
               </div>
 
               {/* Task Field - Full width */}
-              <div>
+              {/* <div>
                 <label
                   htmlFor="task"
                   className="mb-2 ml-1 block text-sm font-semibold text-neutral-700 dark:text-neutral-300"
@@ -430,11 +486,7 @@ function FormSection() {
                   placeholder="Esempi: Inserimento dati nei fogli Excel, invio messaggi di follow-up, aggiornamento CRM..."
                   className="min-h-32 w-full rounded-lg border border-neutral-300 bg-white px-4 py-3 text-neutral-900 placeholder-neutral-400 transition-all focus:border-primary-500 focus:outline-none focus:ring-2 focus:ring-primary-500/20 dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100 dark:placeholder-neutral-500"
                 />
-                {/* <p className="ml-1 mt-2 text-xs text-neutral-500 dark:text-neutral-400">
-                  Aiutami a capire meglio le tue esigenze per darti suggerimenti
-                  più mirati
-                </p> */}
-              </div>
+              </div> */}
 
               {/* Submit Button */}
               <button
